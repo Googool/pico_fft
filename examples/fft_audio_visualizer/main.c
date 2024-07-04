f183a9c35dd4a570256c615481e310ef71c84a5f
Ask Marius Lysgaard <askx0113@gmail.com>
2024-07-04 13:08:44 +0200

    Fixed the example to show the average amplitude of each bin.

--- examples/fft_audio_visualizer/main.c
+++ examples/fft_audio_visualizer/main.c
@@ -11,25 +11,61 @@ frequency_bin_t bins[] = {
   {"Brilliance", 6001, 20000, 0}
 };
 
+#define BIN_COUNT (sizeof(bins) / sizeof(frequency_bin_t))
+
+float freqs[NSAMP];
+
+void calculate_freqs() {
+  float f_max = FSAMP / 2.0;
+  float f_res = f_max / (NSAMP / 2);
+  for (int i = 0; i < NSAMP / 2; i++) {
+    freqs[i] = f_res * i;
+  }
+}
+
 int main() {
-  uint8_t cap_buf[NSAMP];
+  uint8_t capture_buf[NSAMP];
   fft_setup();
+  calculate_freqs();
 
   while (1) {
-    fft_sample(cap_buf);
-    fft_process(cap_buf, bins, sizeof(bins) / sizeof(frequency_bin_t));
-    for (int i = 0; i < sizeof(bins) / sizeof(frequency_bin_t); i++) {
+    fft_sample(capture_buf);
+    fft_process(capture_buf, bins, BIN_COUNT);
+
+    for (int i = 0; i < BIN_COUNT; i++) {
+      bins[i].amplitude = 0;
+    }
+
+    int bin_counts[BIN_COUNT] = {0};
+
+    for (int i = 0; i < NSAMP / 2; i++) {
+      float freq = freqs[i];
+      float amplitude = capture_buf[i];
+
+      for (int j = 0; j < BIN_COUNT; j++) {
+        if (freq >= bins[j].freq_min && freq <= bins[j].freq_max) {
+          bins[j].amplitude += amplitude;
+          bin_counts[j]++;
+          break;
+        }
+      }
+    }
+
+    for (int i = 0; i < BIN_COUNT; i++) {
+      if (bin_counts[i] > 0) {
+        bins[i].amplitude /= bin_counts[i];
+      }
       printf("%s: Amplitude: %f\n", bins[i].name, bins[i].amplitude);
     }
 
-    /* Example:
-    * Sub-bass: Amplitude: 235.854950
-    * Bass: Amplitude: 24.894323
-    * Low Midrange: Amplitude: 23.291584
-    * Midrange: Amplitude: 56.030037
-    * Upper Midrange: Amplitude: 71.086266
-    * Presence: Amplitude: 64.704025
-    * Brilliance: Amplitude: 212.929337
+    /* Example output:
+    * Sub-bass: Amplitude: 59.000000
+    * Bass: Amplitude: 59.500000
+    * Low Midrange: Amplitude: 59.666668
+    * Midrange: Amplitude: 64.000000
+    * Upper Midrange: Amplitude: 69.133331
+    * Presence: Amplitude: 66.043480
+    * Brilliance: Amplitude: 57.255520
     */
 
     sleep_ms(1000);
